16aac9c1d292d808301b6f5af574df656aaa430f
Gustavo Akira Uekita <akirauekita2002@gmail.com>
2021-05-23 11:14:57 -0300

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,9 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('render',()=>{
+  render(<App/>);
+  const nullElement = screen.getByText(/akira/);
+  expect(nullElement).toBeInTheDocument();
+})
